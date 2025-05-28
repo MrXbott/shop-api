a9a17e25ba8df90194d6cf17562b916491638876
Mr.Xbott <mr.xbott@gmail.com>
2025-05-28 12:23:47 +0300

    move crud logic from products routes to a separate module

--- app/api/v1/products.py
+++ app/api/v1/products.py
@@ -1,60 +1,59 @@
 from fastapi import APIRouter, HTTPException
-from bson import ObjectId
-from typing import List
 
-from app.db.database import db
 from app.schemas.products import ProductCreate, ProductUpdate, ProductFromDB
+from app.db.crud import products as crud
 
 router = APIRouter(prefix='/products')
 
 @router.post('/', response_model=ProductFromDB)
-async def create_product( product: ProductCreate):
-    result = await db.products.insert_one( product.model_dump())
-    new_product = await db.products.find_one({'_id': result.inserted_id})
+async def create_product(product: ProductCreate):
+    result = await crud.create_new_product(product.model_dump())
+    if not result.inserted_id:
+        raise HTTPException(500, 'Failed to insert new product')
+    
+    new_product = await crud.get_product_by_id(result.inserted_id)
     if not new_product:
         raise HTTPException(500, 'Failed to fetch inserted product')
-    return new_product
+    return ProductFromDB(**new_product)
 
-@router.get('/', response_model=List[ProductFromDB], response_model_by_alias=False)
+@router.get('/', response_model=list[ProductFromDB], response_model_by_alias=False)
 async def get_all_products():
-    result = db.get_collection('products').find()
-    products = []
-    async for product in result:
-        products.append(ProductFromDB(**product))
-    return products
+    products = await crud.get_products()
+    return [ProductFromDB(**product) for product in products]
 
 @router.get('/{id}', response_model=ProductFromDB, response_model_by_alias=False)
 async def get_product_by_id(id: str):
-    product = await db.products.find_one({'_id': ObjectId(id)})
+    product = await crud.get_product_by_id(id)
     if not product:
         raise HTTPException(404, 'Product not found')
-    return product
+    return ProductFromDB(**product)
 
 @router.put('/{id}', response_model=ProductFromDB, response_model_by_alias=False)
 async def update_product_full(id: str, product: ProductCreate):
-    result = await db.products.replace_one({'_id': ObjectId(id)}, product.model_dump())
+    result = await crud.update_product_full(id, product.model_dump())
     if result.matched_count == 0:
         raise HTTPException(404, detail='Product not found')  
-    updated_product = await db.products.find_one({'_id': ObjectId(id)})
+
+    updated_product = await crud.get_product_by_id(id)
     if not updated_product:
         raise HTTPException(500, detail='Failed to fetch updated product')
     return updated_product
 
 @router.patch('/{id}', response_model=ProductFromDB, response_model_by_alias=False)
 async def update_product_partial(id: str, product: ProductUpdate):
-    obj_id = ObjectId(id)
     update_data = {k: v for k, v in product.model_dump().items() if v not in (None, [], {})}
-    result = await db.products.update_one({'_id': obj_id}, {'$set': update_data})
+    result = await crud.update_product_partial(id, update_data)
     if result.matched_count == 0:
         raise HTTPException(404, detail='Product not found')
-    updated_product = await db.products.find_one({'_id': obj_id})
+    
+    updated_product = await crud.get_product_by_id(id)
     if not updated_product:
         raise HTTPException(500, detail='Failed to fetch updated product')
     return updated_product
 
 @router.delete('/{id}')
 async def delete_product_by_id(id: str):
-    result = await db.products.delete_one({'_id': ObjectId(id)})
+    result = await crud.delete_product_by_id(id)
     if result.deleted_count == 0:
         raise HTTPException(404, detail='Product not found')
     return {'message': 'Product deleted'}
