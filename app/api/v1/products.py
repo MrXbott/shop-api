529cfdb1973e3e5ac01539c9444e9b3da5455489
Mr.Xbott <mr.xbott@gmail.com>
2025-06-03 12:08:34 +0300

    change routes order

--- app/api/v1/products.py
+++ app/api/v1/products.py
@@ -6,21 +6,14 @@ from app.auth.dependencies import get_admin_user
 
 router = APIRouter(prefix='/products')
 
+# ---- admin routes
 @router.post('/', response_model=ProductFromDB, response_model_by_alias=False, status_code=status.HTTP_200_OK, dependencies=[Depends(get_admin_user)])
 async def create_product(product: ProductCreate):
     return await Product.create(product)
 
-@router.get('/', response_model=list[ProductFromDB], response_model_by_alias=False, status_code=status.HTTP_200_OK)
-async def get_products(params: ProductQueryParams = Depends()):
-    return await Product.get_by_params(params)
-
 @router.get('/count', status_code=status.HTTP_200_OK, dependencies=[Depends(get_admin_user)])
 async def get_products_count():
     return {'products_count': await Product.count()}
-
-@router.get('/{product_id}', response_model=ProductFromDB, response_model_by_alias=False, status_code=status.HTTP_200_OK)
-async def get_product_by_id(product_id: str):
-    return await Product.get_by_id(product_id)
 
 @router.put('/{product_id}', response_model=ProductFromDB, response_model_by_alias=False, status_code=status.HTTP_200_OK, dependencies=[Depends(get_admin_user)])
 async def update_product_full(product_id: str, product_data: ProductCreate):
@@ -34,3 +27,15 @@ async def update_product_partial(product_id: str, product_data: ProductUpdate):
 async def delete_product_by_id(product_id: str):
     await Product.delete(product_id)
     return {'message': 'Product deleted'}
+
+
+# ---- open routes
+@router.get('/', response_model=list[ProductFromDB], response_model_by_alias=False, status_code=status.HTTP_200_OK)
+async def get_products(params: ProductQueryParams = Depends()):
+    return await Product.get_by_params(params)
+
+@router.get('/{product_id}', response_model=ProductFromDB, response_model_by_alias=False, status_code=status.HTTP_200_OK)
+async def get_product_by_id(product_id: str):
+    return await Product.get_by_id(product_id)
+
+
