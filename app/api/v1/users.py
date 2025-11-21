14afe0cfa72707bc57a6a0fac0987ac30680ee12
Mr.Xbott <mr.xbott@gmail.com>
2025-11-21 21:12:29 +0300

    update user endpoints

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -8,13 +8,6 @@ from app.exceptions.users import UserNotFound, CreateUserException, UpdateUserEx
 
 
 router = APIRouter(prefix='/users')
-
-@router.get('/{user_id}', response_model=UserFromDB, status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)])
-async def get_user_profile_by_id(user_id: int, user_service: UserService = Depends(get_user_service)):
-    try:
-        return await user_service.get_user_by_id(user_id)
-    except UserNotFound as e:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, str(e))
     
 @router.get('/me', response_model=UserProfile, response_model_by_alias=False, status_code=status.HTTP_200_OK)
 async def get_my_profile(current_user: UserFromDB = Depends(get_current_user)):
@@ -37,12 +30,20 @@ async def create_user(user: UserRegisterByAdmin, user_service: UserService = Dep
         raise HTTPException(e.status_code, e.message)
 
 @router.get('/', response_model=list[UserFromDB], status_code=status.HTTP_200_OK, dependencies=[Depends(get_admin_user)])
-async def get_users(params: UserQueryParams = Depends(), user_service: UserService = Depends(get_user_service)):
+async def get_users_by_params(params: UserQueryParams = Depends(), user_service: UserService = Depends(get_user_service)):
     return await user_service.get_users_by_params(params)
 
 @router.get('/count', status_code=status.HTTP_200_OK, dependencies=[Depends(get_admin_user)])
 async def get_users_count(user_service: UserService = Depends(get_user_service)):
     return {'users_count': await user_service.count_users()}
+
+
+@router.get('/{user_id}', response_model=UserFromDB, status_code=status.HTTP_200_OK, dependencies=[Depends(get_admin_user)])
+async def get_user_profile_by_id(user_id: int, user_service: UserService = Depends(get_user_service)):
+    try:
+        return await user_service.get_user_by_id(user_id)
+    except UserNotFound as e:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, str(e))
 
 @router.patch('/{user_id}', response_model=UserFromDB, status_code=status.HTTP_200_OK, dependencies=[Depends(get_admin_user)])
 async def update_user_profile_by_id(user_id: int, user_data: UserUpdate, user_service: UserService = Depends(get_user_service)):
